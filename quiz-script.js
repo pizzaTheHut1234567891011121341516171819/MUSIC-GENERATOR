a44fd323bac5bb8bf4a773f8652c799fc7cb0aeb
pizzaTheHut1234567891011121341516171819 <40699673+pizzaTheHut1234567891011121341516171819@users.noreply.github.com>
2018-06-29 13:13:14 -0400

    Add files via upload

---
+++ quiz-script.js
@@ -0,0 +1,109 @@
+
+$(document).ready(function(){
+    $("#button-c").click(function(){
+        $(".q2").load("q2.html");
+        console.log(3);
+    });
+  
+    
+     $("#B1").click(function(){
+        $(".q2").load("q2_1.html");
+        console.log(3);
+    });  
+    $("#B2").click(function(){
+        $(".q2").load("q2_2.html");
+        console.log(3);
+    });  
+    $("#B4").click(function(){
+        $(".q2").load("q2_4.html");
+        console.log(3);
+    }); 
+   
+    
+    
+});
+    
+  
+
+ $("#BA").click(function(){
+        $(".song").load("Opera.html");
+        console.log(3);
+    }); 
+ $("#BB").click(function(){
+        $(".song").load("Orchestra.html");
+        console.log(3);
+    }); 
+ $("#BC").click(function(){
+        $(".song").load("piano.html");
+        console.log(3);
+    }); 
+ $("#BD").click(function(){
+        $(".song").load("choral.html");
+        console.log(3);
+    }); 
+ $("#BF").click(function(){
+        $(".song").load("rap.html");
+        console.log(3);
+    }); 
+ $("#BG").click(function(){
+        $(".song").load("Kpop.html");
+        console.log(3);
+    }); 
+ $("#BH").click(function(){
+        $(".song").load("pop.html");
+        console.log(3);
+    }); 
+ $("#BI").click(function(){
+        $(".song").load("R&B.html");
+        console.log(3);
+    }); 
+ $("#BJ").click(function(){
+        $(".song").load("punk.html");
+        console.log(3);
+    }); 
+ $("#BK").click(function(){
+        $(".song").load("alternative.html");
+        console.log(3);
+    }); 
+       
+ $("#BL").click(function(){
+        $(".song").load("heavymetal.html");
+        console.log(3);
+    }); 
+ $("#BM").click(function(){
+        $(".song").load("classic.html");
+        console.log(3);
+    }); 
+ $("#BN").click(function(){
+        $(".song").load("smooth.html");
+        console.log(3);
+    }); 
+ $("#BO").click(function(){
+        $(".song").load("blues.html");
+        console.log(3);
+    }); 
+ $("#BP").click(function(){
+        $(".song").load("ragtime.html");
+        console.log(3);
+    }); 
+ $("#BQ").click(function(){
+        $(".song").load("bigband.html");
+        console.log(3);
+    }); 
+ $("#B4").click(function(){
+        $(".q2").load("q2_4.html");
+        console.log(3);
+    }); 
+
+
+
+
+
+
+
+
+
+
+
+
+
